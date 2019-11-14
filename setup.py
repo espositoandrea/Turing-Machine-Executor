0dfbacc74a015cbb0dc05fbb7453228c37b6f694
Andrea Esposito <esposito_andrea99@hotmail.com>
2019-11-14 19:44:25 +0100

    Uploaded to PyPi
    Link to PyPi:
    https://pypi.org/project/turing-machine-executor/

--- setup.py
+++ setup.py
@@ -11,10 +11,13 @@ setup(
     description="A Turing Machine executor",
     license="MIT",
     long_description=long_description,
+    long_description_content_type='text/markdown',
     author='Andrea Esposito',
-    author_email='andrea.esposito099@gmail.com',
+    author_email='esposito_andrea99@hotmail.com',
     url="https://github.com/espositoandrea/Turing-Machine-Executor",
     packages=['turing_machine_executor'],
+    download_url='https://github.com/espositoandrea/Turing-Machine-Executor/archive/v1.1.0-alpha.tar.gz',
+    keywords=['Turing', 'Machine', 'Executor', 'EMT'],
     install_requires=["colorama", "pyfiglet"],
     entry_points={
         'console_scripts': [
@@ -22,4 +25,14 @@ setup(
             'turing-machine-executor = turing_machine_executor.__main__:run',
         ],
     },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Education',
+        'Topic :: Education',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+    ],
 )
